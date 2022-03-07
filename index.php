6438f198ebae2dd881c5d380efa1e6196264e9f7
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-03-07 21:41:32 +0100

    bootstrap include helper

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <?php include "bootstrap.html"; ?>
+</head>
+<body>
+<?php
+include "navBar.html";
+?>
+</body>
+</html>
