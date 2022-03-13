072b8aa9cd7895b46f08ee02b498a230023d4e30
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-03-13 15:58:22 +0100

    Add login fronted validation.

--- index.php
+++ index.php
@@ -10,15 +10,33 @@
 </head>
 <body class="bg-dark text-light">
 <div class="container">
-    <form action="main.php" method="get">
+    <form action="main.php" class="needs-validation" method="get" novalidate>
         <div class="my-3">
             <label for="email" class="form-label">Adres email</label>
-            <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+            <input type="email" class="form-control border-2" id="email" name="email" aria-describedby="emailHelp"
+                   required>
+            <div class="valid-feedback">Adres email spełnia wszystkie warunki.</div>
+            <div class="invalid-feedback">Adres email jest niepoprawny. Adres email musi zawierać @ oraz domenę,
+                na której jest zarejestrowany. (przykłądowyAdresEmail@mojastrona.pl)
+            </div>
             <div id="emailHelp" class="form-text">Nie podawaj nigdy, nikomu adresu email ani hasła.</div>
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Hasło</label>
-            <input type="password" class="form-control" id="password" name="password">
+            <input type="password" class="form-control border-2" id="password" name="password"
+                   required autocomplete="current-password"
+                   pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,60}$">
+            <div class="valid-feedback">Hasło spełnia wszystkie warunki</div>
+            <div class="invalid-feedback">
+                Hasło musi się składać z:
+                <ul>
+                    <li>Conajmniej jednej małej litery.</li>
+                    <li>Conajmniej jednej dużej litery.</li>
+                    <li>Conajmniej jednej cyfry.</li>
+                    <li>Conajmniej jednego znaku specjalnego (! @ # $ % ^ & * _ = + -).</li>
+                    <li>Musi składać się z minimalnie 8 znaków, a maksymalnie 60 znaków.</li>
+                </ul>
+            </div>
         </div>
         <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="noLogout" name="noLogout">
@@ -27,5 +45,6 @@
         <button type="submit" class="btn btn-primary">Zaloguj się</button>
     </form>
 </div>
+<script src="login.js"></script>
 </body>
 </html>
---
+++ login.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//Pobiera wszystkie potencjalne formularze, które trzeba zwalidować.
+const forms = document.querySelectorAll('.needs-validation');
+
+//Iteruje po kolei po każdym formularzu znajdującym się na stronie internetowej
+for (const form of forms) {
+
+    //Dodaje słuchacza, który nasłuchuje, czy formularz został wysłany.
+    form.addEventListener('submit', event => {
+
+        //Sprawdzenie, czy dany element formularza spełnia wszystkie warunki (określone w HTML).
+        if (!form.checkValidity()) {
+            //Zapobiega wysłaniu formularza.
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    })
+    form.classList.add('was-validated')
+}
+
