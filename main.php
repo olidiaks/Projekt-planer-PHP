5f337da20ce2c05b072f3b2ed2829351687b5ff6
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-03-17 20:24:12 +0100

    add frontend to show plan

--- main.php
+++ main.php
@@ -13,28 +13,50 @@ if ($_POST && isset($_POST['email']) && isset($_POST['password'])) {
     include 'database/database connection.php';
 
     $sql = "select idUser from Users where email = '$email' and password = '$password'";
-
     $query = mysqli_query($con, $sql);
 
     if (mysqli_num_rows($query) == 1) {
-        $idUser = mysqli_fetch_array($query)['idUser'];
-
-        $_SESSION['idUser'] = $idUser;
+        $_SESSION['idUser'] = mysqli_fetch_array($query)['idUser'];
 
         include "navBar.php";
 
-        for ($i = 0; $i < 28; $i++) {
-            echo $date = date("Y-m-d", time() + 86400 * $i) . '<br>';
-            $sql = "select content, time from Event
-                    where date = '$date' and idUser = '" . $_SESSION['idUser'] . "' order by time";
-            $query = mysqli_query($con, $sql);
-            while ($row = mysqli_fetch_array($query)) {
+        echo '<div class="container mt-2">';
 
+        for ($i = 0; $i < 30; $i++) {
+            if ($i % 3 == 0) {
+                echo '<div class="row bg-gradient">';
+
+            }
+            $date = date("Y-m-d", time() + 86400 * $i) . '<br>';
+
+            $sql = "select content, timeStart, timeEnd from Event
+                    where date = '$date' and idUser = '" . $_SESSION['idUser'] . "'
+                    order by timeStart";
+            $query = mysqli_query($con, $sql);
+            echo "<div class='col-12 col-md-4 border border-4'>
+                      <div class='row'>
+                            <div class='col bg-info text-black'>Plan na dzień $date</div>
+                      </div>";
+            while ($row = mysqli_fetch_array($query)) {
+                echo '<div class="row">
+                          <div class="col">
+                              <div class="row">
+                                  <div class="col bg-primary">Wydążenie rozpoczyna się o' . $row['timeStart'] . ', zakończy się o ' . $row['timeEnd'] . '.</div>
+                              </div>
+                              <div class="row">
+                                  <div class="col bg-secondary">' . $row['content'] . '</div>
+                              </div>
+                          </div>
+                      </div>';
+            }
+            echo "</div>";
+            if ($i % 3 == 2) {
+                echo '</div>';
             }
 
         }
 
-
+        echo '</div>';
         ?>
 
         <script src="navbar.js"></script>
