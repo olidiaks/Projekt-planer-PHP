1e8ae69ff68443c982caf5ae9b39178f58af105f
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-03-17 11:20:10 +0100

    Add login module backend.

--- main.php
+++ main.php
@@ -4,16 +4,43 @@
 
 <body class="bg-dark text-light">
 <?php
-include "navBar.html";
 
-include 'database/database connection.php';
+if ($_POST && isset($_POST['email']) && isset($_POST['password'])) {
 
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-?>
+    include 'database/database connection.php';
 
-<script src="navbar.js"></script>
-<script>
-    activeItemOnNavbar('Strona główna');
-</script>
+    $sql = "select idUser from Users where email = '$email' and password = '$password'";
+
+    $query = mysqli_query($con, $sql);
+
+    if (mysqli_num_rows($query) == 1) {
+        $idUser = mysqli_fetch_array($query)['idUser'];
+
+        $_SESSION['idUser'] = $idUser;
+
+        include "navBar.php";
+        ?>
+
+        <script src="navbar.js"></script>
+        <script>
+            activeItemOnNavbar('Strona główna');
+        </script>
+    <?php } else {
+        echo '<div class="container">
+                <div class="alert alert-danger mt-3">Nie ma konta o takim e–mailu lub haśle.</div>
+            </div>';
+    }
+    mysqli_close($con);
+} else {
+    echo '<div class="container">
+                <div class="alert alert-danger mt-3">
+                    Nie ma wszystkich potrzebnych danych potrzebnych do zalogowania się.
+                </div>
+           </div>';
+    include 'index.php';
+} ?>
 </body>
 </html>
--- navBar.php
+++ navBar.php
@@ -6,12 +6,20 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="navbar-collapse collapse" id="navbar">
-            <ul class="navbar-nav">
+        <div class="navbar-collapse collapse " id="navbar">
+            <ul class="navbar-nav me-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="main.php">Strona główna</a>
                 </li>
             </ul>
+            <div class="navbar-text">
+                Witaj
+                <?php
+                $sql = "select userName from Users where idUser = '" . $_SESSION['idUser'] . "'";
+                $query = mysqli_query($con, $sql);
+                echo mysqli_fetch_array($query)['userName'];
+                ?>
+            </div>
         </div>
     </div>
 </nav>
