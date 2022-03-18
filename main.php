378d28686debb653bf337ad807709ed0aed08caa
Oliwier Stępniak <oliwierstepniak@gmail.com>
2022-03-18 18:41:16 +0100

    Making sure the user is logged if  his id is stored in a session. Not from coming in  the login form .

--- main.php
+++ main.php
@@ -4,6 +4,7 @@
 
 <body class="bg-dark text-light">
 <?php
+session_start();
 
 if ($_POST && isset($_POST['email']) && isset($_POST['password'])) {
 
@@ -18,27 +19,38 @@ if ($_POST && isset($_POST['email']) && isset($_POST['password'])) {
     if (mysqli_num_rows($query) == 1) {
         $_SESSION['idUser'] = mysqli_fetch_array($query)['idUser'];
 
-        include "navBar.php";
+    } else {
+        echo '<div class="container">
+                <div class="alert alert-danger mt-3">Nie ma konta o takim e–mailu lub haśle.</div>
+              </div>';
 
-        echo '<div class="container mt-2">';
+    }
+    mysqli_close($con);
+}
 
-        for ($i = 0; $i < 30; $i++) {
-            if ($i % 3 == 0) {
-                echo '<div class="row bg-gradient">';
+if (isset($_SESSION['idUser'])) {
+    include "database/database connection.php";
+    include "navBar.php";
 
-            }
-            $date = date("Y-m-d", time() + 86400 * $i) . '<br>';
+    echo '<div class="container mt-2">';
 
-            $sql = "select content, timeStart, timeEnd from Event
+    for ($i = 0; $i < 30; $i++) {
+        if ($i % 3 == 0) {
+            echo '<div class="row bg-gradient">';
+
+        }
+        $date = date("Y-m-d", time() + 86400 * $i) . '<br>';
+
+        $sql = "select content, timeStart, timeEnd from Event
                     where date = '$date' and idUser = '" . $_SESSION['idUser'] . "'
                     order by timeStart";
-            $query = mysqli_query($con, $sql);
-            echo "<div class='col-12 col-md-4 border border-4'>
+        $query = mysqli_query($con, $sql);
+        echo "<div class='col-12 col-md-4 border border-4'>
                       <div class='row border-bottom border-4'>
                             <div class='col bg-info text-black'>Plan na dzień $date</div>
                       </div>";
-            while ($row = mysqli_fetch_array($query)) {
-                echo '<div class="row">
+        while ($row = mysqli_fetch_array($query)) {
+            echo '<div class="row">
                           <div class="col">
                               <div class="row">
                                   <div class="col bg-primary border-bottom border-4">Wydążenie rozpoczyna się o' . $row['timeStart'] . ', zakończy się o ' . $row['timeEnd'] . '.</div>
@@ -48,34 +60,29 @@ if ($_POST && isset($_POST['email']) && isset($_POST['password'])) {
                               </div>
                           </div>
                       </div>';
-            }
-            echo "</div>";
-            if ($i % 3 == 2) {
-                echo '</div>';
-            }
-
+        }
+        echo "</div>";
+        if ($i % 3 == 2) {
+            echo '</div>';
         }
 
-        echo '</div>';
-        ?>
-
-        <script src="navbar.js"></script>
-        <script>
-            activeItemOnNavbar('Strona główna');
-        </script>
-    <?php } else {
-        echo '<div class="container">
-                <div class="alert alert-danger mt-3">Nie ma konta o takim e–mailu lub haśle.</div>
-              </div>';
     }
-    mysqli_close($con);
+
+    echo '</div>';
+    ?>
+
+    <script src="navbar.js"></script>
+    <script>
+        activeItemOnNavbar('Strona główna');
+    </script>
+    <?php
 } else {
     echo '<div class="container">
                 <div class="alert alert-danger mt-3">
-                    Nie ma wszystkich potrzebnych danych potrzebnych do zalogowania się.
+                    Nie jesteś zalogowany
                 </div>
            </div>';
-    include 'index.php';
+    include "index.php";
 } ?>
 </body>
 </html>
